6b0cac3cc2626fd20903d6ca04d2bef6208b4338
WooHCode <gusdn526@naver.com>
2022-12-21 00:01:54 +0900

    feature card, cart

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -9,7 +9,9 @@
                 </span>
             </p>
             <div class="d-flex justify-content-between align-items-center">
-                <button class="btn btn-primary">구입하기</button>
+                <button class="btn btn-primary" @click="addToCart(item.id)">
+                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                </button>
                 <small class="price text-muted">
                     {{ lib.getNumerFormatted(item.price) }}원
                 </small>
@@ -22,13 +24,21 @@
 </template>
 <script>
 import lib from "@/scripts/lib";
+import axios from 'axios';
+
 export default {
     name: "Card",
     props: {
         item: Object
     },
     setup() {
-        return { lib }
+        const addToCart = (itemId) => {
+            axios.post(`/api/cart/items/${itemId}`).then(() => {
+                console.log('success')
+            })
+        };
+
+        return { lib, addToCart }
     }
 }
 </script>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,23 +1,25 @@
 <template>
     <div class="form-signin w-100 m-auto">
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-            <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="state.form.email">
-                <label for="floatingInput">Email address</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="state.form.password">
-                <label for="floatingPassword">Password</label>
-            </div>
+        <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+                v-model="state.form.email">
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                v-model="state.form.password">
+            <label for="floatingPassword">Password</label>
+        </div>
 
-            <div class="checkbox mb-3">
-                <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                </label>
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" @click="submit()">Sign in</button>
-            <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" @click="submit()">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
     </div>
 </template>
 
@@ -29,25 +31,25 @@ import router from '@/scripts/router'
 export default {
     setup() {
         const state = reactive({
-            form :{
+            form: {
                 email: '',
                 password: '',
             }
-    })
-
-    const submit =()=>{
-        axios.post("/api/account/login", state.form).then((res)=>{
-            store.commit("setAccount", res.data)
-            console.log(res.data)
-            sessionStorage.setItem("id", res.data)
-            router.push({path: '/'})
-            window.alert("로그인하였습니다.")
-        }).catch(()=>{
-            window.alert("로그인 정보가 없습니다.")
         })
-    }
 
-    return {state,submit}
+        const submit = () => {
+            axios.post("/api/account/login", state.form).then((res) => {
+                store.commit("setAccount", res.data)
+                console.log(res.data)
+                sessionStorage.setItem("id", res.data)
+                router.push({ path: '/' })
+                window.alert("로그인하였습니다.")
+            }).catch(() => {
+                window.alert("로그인 정보가 없습니다.")
+            })
+        }
+
+        return { state, submit }
     },
 }
 </script>
